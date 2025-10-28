fc942e3032bf0700275e068b7769ec3bb16db831
Isaac Hughes <isaac@legnasoftware.com>
2025-10-28 08:59:42 -0400

    Long feature commit 1 - adds for loop

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
 console.log("Hello, world!")
 
 console.log("Dev commit 1")
+
+console.log("Long feature changes")
+for (let i = 0; i < 10; i++) {
+  console.log("Long feature change number: " + i)
+}
